902b652f0babb1bf0229bcf979a80c61bdc6fd92
codethinki <lukas.jmy@outlook.de>
2026-01-18 15:58:10 +0100

    asdf

--- cth_assertions.cmake
+++ cth_assertions.cmake
@@ -6,58 +6,58 @@ macro(_cth_assertion_failure reason)
     message(FATAL_ERROR "ERROR: ${reason} [args: ${ARGN}]")
 endmacro()
 
-# cth_assert_if(<condition> <reason> <args...>)
+# cth_assert_if(<reason> <condition...>)
 # pre: condition is a cmake bool expression
 # post: condition is TRUE
-function(cth_assert_if condition reason)
-    if(NOT (${condition}))
-        _cth_assertion_failure("${reason}" ${ARGN})
+macro(cth_assert_if reason)
+    if(NOT ${ARGN})
+        _cth_assertion_failure("${reason}")
     endif()
-endfunction()
+endmacro()
 
-# cth_assert_if_not(<condition> <reason> <args...>)
+# cth_assert_if_not(<reason> <condition...>)
 # pre: condition is a cmake bool expression
 # post: condition is FALSE
-function(cth_assert_if_not condition reason)
-    if(${condition})
-        _cth_assertion_failure("${reason}" ${ARGN})
+macro(cth_assert_if_not reason)
+    if(${ARGN})
+        _cth_assertion_failure("${reason}")
     endif()
-endfunction()
+endmacro()
 
 # cth_assert_not_cmd(<cmd>)
 # post: cmd is NOT a defined command/function/macro
 function(cth_assert_not_cmd cmd)
-    cth_assert_if_not(COMMAND ${cmd} "Command '${cmd}' already defined")
+    cth_assert_if_not("Command '${cmd}' already defined" COMMAND ${cmd})
 endfunction()
 
 # cth_assert_cmd(<cmd>)
 # post: cmd is a defined command/function/macro
 function(cth_assert_cmd cmd)
-    cth_assert_if(COMMAND ${cmd} "Command '${cmd}' not defined")
+    cth_assert_if("Command '${cmd}' not defined" COMMAND ${cmd})
 endfunction()
 
 # cth_assert_target(<target>)
 # post: target exists
 function(cth_assert_target target)
-    cth_assert_if(TARGET ${target} "Target '${target}' does not exist")
+    cth_assert_if("Target '${target}' does not exist" TARGET ${target})
 endfunction()
 
 # cth_assert_not_target(<target>)
 # post: target does NOT exist
 function(cth_assert_not_target target)
-    cth_assert_if_not(TARGET ${target} "Target '${target}' already exists")
+    cth_assert_if_not("Target '${target}' already exists" TARGET ${target})
 endfunction()
 
 # cth_assert_empty(<value>)
 # post: value is an empty string
 function(cth_assert_empty value)
-    cth_assert_if("\"${value}\" STREQUAL \"\"" "Value not empty: '${value}'")
+    cth_assert_if("Value not empty: '${value}'" "${value}" STREQUAL "")
 endfunction()
 
 # cth_assert_not_empty(<value>)
 # post: value is NOT an empty string
 function(cth_assert_not_empty value)
-    cth_assert_if_not("\"${value}\" STREQUAL \"\"" "Value is empty")
+    cth_assert_if_not("Value is empty" "${value}" STREQUAL "")
 endfunction()
 
 # cth_assert_program(<prog>)
@@ -68,7 +68,7 @@ function(cth_assert_program prog)
     
     find_program(${VAR_NAME} "${prog}")
     
-    cth_assert_if("${VAR_NAME}" "Program '${prog}' not found")
+    cth_assert_if("Program '${prog}' not found" ${VAR_NAME})
     
     set(${VAR_NAME} "${${VAR_NAME}}" PARENT_SCOPE)
 endfunction()
