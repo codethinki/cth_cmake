a0dbe453d578885d320568729eb625ef9e7ae47b
codethinki <lukas.jmy@outlook.de>
2026-01-19 20:07:55 +0100

    asdf

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 4.0.0)
 
-message(STATUS "----cth_cmake toolchain----")
+message(STATUS "---- cth_cmake toolchain ----")
 
 #append cmake dir to module path
 set(CTH_CMAKE_LIBRARY_DIR ${CMAKE_CURRENT_LIST_DIR})
@@ -22,5 +22,6 @@ if(NOT CTH_DISABLE_VCPKG_INTEGRATION)
     cth_assert_program(vcpkg)
     cth_assert_not_empty("$ENV{VCPKG_ROOT}")
 
+    message(STATUS "handing off to vcpkg")
     include("$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
 endif()
