48c13ed914b1272f466a57da3b3db2324569bad4
codethinki <lukas.jmy@outlook.de>
2026-01-19 20:00:11 +0100

    trying to force the module path update

--- toolchain.cmake
+++ toolchain.cmake
@@ -3,6 +3,7 @@ cmake_minimum_required(VERSION 4.0.0)
 #append cmake dir to module path
 set(CTH_CMAKE_LIBRARY_DIR ${CMAKE_CURRENT_LIST_DIR})
 list(APPEND CMAKE_MODULE_PATH "${CTH_CMAKE_LIBRARY_DIR}")
+set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" CACHE STRING "Search path for CMake modules" FORCE)
 message(STATUS "Appended ${CTH_CMAKE_LIBRARY_DIR} to module path")
 
 include(cth_assertions)
